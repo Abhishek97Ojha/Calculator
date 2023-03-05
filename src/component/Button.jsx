7ac289b5973b20417125fc8f78bcc34054c60e8f
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-03-05 16:54:59 +0530

    calculator

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Button.css'
-const Button = ({width,height,name,bgcolor}) => {
+const Button = ({width,height,name,bgcolor,onclick,arg}) => {
   return (
     <div className='button'>
-        <button style={{width:`${width}`,height:`${height}`,backgroundColor:`${bgcolor}`}}>{name}</button>
+        <button onClick={()=>{
+            console.log("clicked");
+           onclick({arg});
+        }} style={{width:`${width}`,height:`${height}`,backgroundColor:`${bgcolor}`}}>{name}</button>
     </div>
   )
 }
--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Display.css'
+import { MyContext } from '../App'
 const Display = () => {
+    const context = useContext(MyContext);
   return (
-    <div className='display'>0</div>
+    <div className='display'>
+        <div className="top">
+            {context.num1}
+        </div>
+        <div className="bottom" style={{fontSize:"1.5rem", color:(context.ans!==0)?"#ffa500":"white"}}>{context.ans}</div>
+    </div>
   )
 }
 
